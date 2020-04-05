40f134a117013ba8208d3f1adc3ed754dd9f9988
Arpit Banati <banatiarpit97@yahoo.co.in>
2020-04-06 02:25:52 +0530

    Initial dist subtree commit

--- src/components/PokemonType/PokemonType.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let name;
-</script>
-
-<style src="./PokemonType.styles.scss"></style>
-
-<li class={`type ${name}`}>
- {name}
-</li>
--- src/store/pokemonList.js
+++
@@ -1,7 +0,0 @@
-import {writable} from 'svelte/store';
-
-export const pokemonListStore = writable({
-    pokemons: [],
-    activeIndex: 1,
-    fetchedAll: false
-});
